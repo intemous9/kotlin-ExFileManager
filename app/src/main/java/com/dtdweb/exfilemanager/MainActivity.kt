e6db16c0016e0a9470b0ad779c1924da1a2caa46
muroi <intemous9@gmail.com>
2020-04-08 16:43:39 +0900

    ストレージへのアクセス権限を取得するためのリクエスト処理追加

--- app/src/main/java/com/dtdweb/exfilemanager/MainActivity.kt
+++ app/src/main/java/com/dtdweb/exfilemanager/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.dtdweb.exfilemanager
 
+import android.Manifest
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 
@@ -16,6 +20,13 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private val TAG = MainActivity::class.java.simpleName
+
+        private const val REQUEST_EXTERNAL_STORAGE: Int = 1001
+        private val PERMISSIONS_STORAGE: Array<String> = arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+
         private const val ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION"
     }
 
@@ -55,6 +66,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        requestExternalStoragePermission()
+
         manager = getSystemService(Context.USB_SERVICE) as UsbManager
 
         val permissionIntent = PendingIntent.getBroadcast(this, 0, Intent(ACTION_USB_PERMISSION), 0)
@@ -83,6 +96,15 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    private fun requestExternalStoragePermission() {
+        val perm = ContextCompat.checkSelfPermission(
+            this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        if (perm != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE)
+        }
     }
 
     private fun setMessage(mess: String) {
